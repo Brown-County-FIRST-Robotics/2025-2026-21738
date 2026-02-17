c04a56cac3ee7801a7d7b195ef7f564bea797010
Huck1111 <askhuckwood@gmail.com>
2026-02-17 17:04:15 -0600

    limelight working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BuildConstents.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BuildConstents.java
@@ -7,12 +7,12 @@ public final class BuildConstents {
 	public static final String MAVEN_GROUP = "FtcRobotController";
 	public static final String MAVEN_NAME = "TeamCode";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 108;
-	public static final String GIT_SHA = "a5cba5dbdd68e3e38b7a362c460512c786857007";
-	public static final String GIT_DATE = "2026-02-17T00:18:23Z";
+	public static final int GIT_REVISION = 109;
+	public static final String GIT_SHA = "e40bb4ac5e1f0a91ec3c7c0e6ed14864e921b3e0";
+	public static final String GIT_DATE = "2026-02-17T22:48:13Z";
 	public static final String GIT_BRANCH = "master";
-	public static final String BUILD_DATE = "2026-02-17T22:46:57Z";
-	public static final long BUILD_UNIX_TIME = 1771368417499L;
+	public static final String BUILD_DATE = "2026-02-17T22:58:22Z";
+	public static final long BUILD_UNIX_TIME = 1771369102591L;
 	public static final int DIRTY = 1;
 
 	private BuildConstents(){}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelightTargetingApriltags.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.seattlesolvers.solverslib.controller.PIDController;
+
+@TeleOp(name="Limelight Targeting apriltags")
+public class limelightTargetingApriltags extends LinearOpMode {
+    private Limelight3A limelight;
+    private boolean complete;
+    @Override
+    public void runOpMode() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(0);
+
+        DcMotorEx frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        DcMotorEx frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        DcMotorEx backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        DcMotorEx backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+        PIDController pid = new PIDController(0.04, 0, 0.2);
+        pid.setSetPoint(0);
+        pid.setTolerance(2.5);
+        limelight.start();
+        waitForStart();
+        while (opModeIsActive()) {
+            LLResult result = limelight.getLatestResult();
+            double right = 0;
+            if (result != null) {
+                if (result.isValid()) {
+                    telemetry.addData("tx", result.getTx());
+                    right = pid.calculate(result.getTx());
+                    telemetry.update();
+                }else {
+                    telemetry.addLine("No Target.");
+                }
+            }else {
+                telemetry.addLine("No Target.");
+            }
+            if (pid.atSetPoint() || complete) {
+                telemetry.addLine("Complete.");
+                frontLeft.setPower(0);
+                frontRight.setPower(0);
+                backLeft.setPower(0);
+                backRight.setPower(0);
+                complete = true;
+            }
+            if (!complete) {
+                double frontLeftPower = -right;
+                double frontRightPower = right;
+                double backLeftPower = -right;
+                double backRightPower = right;
+
+                double max;
+                max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
+                max = Math.max(max, Math.abs(backLeftPower));
+                max = Math.max(max, Math.abs(backRightPower));
+
+                if (max > 1.0) {
+                    frontLeftPower /= max;
+                    frontRightPower /= max;
+                    backLeftPower /= max;
+                    backRightPower /= max;
+                }
+
+                frontLeft.setPower(frontLeftPower);
+                frontRight.setPower(frontRightPower);
+                backLeft.setPower(backLeftPower);
+                backRight.setPower(backRightPower);
+            }
+            telemetry.update();
+        }
+        limelight.stop();
+    }
+}
